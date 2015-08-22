86da510311e2940d91cc660dff33ebc15b47c215
gnonio <gamma.stardust@gmail.com>
2015-08-22 01:53:13 +0100

    Code refactor - v0.2.0
    - Minor version bump
    - Added getStageByName() function to glCompute
    - Added updateShader() function to glComputeStage
    - Demo interface improved
    - Demo simplified, removed obsolete code
    - Demo shader loading is now dynamic

--- demo/js/vertStageA.glsl
+++
@@ -1,8 +0,0 @@
-attribute vec2 position;
-varying vec2 uv;
-void main() {
-	gl_Position = vec4(position, 0.0, 1.0);
-	//uv = vec2(0.0,1.0) + vec2(0.5,-0.5) * (position + 1.0);
-	//uv = vec2(position);
-	uv = 0.5 * (position+1.0);
-}
